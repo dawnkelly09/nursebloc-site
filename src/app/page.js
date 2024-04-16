8869a182ed971a37e2e1f0ca2bb611946bfc63aa
Dawn Kelly <dawnkelly09@gmail.com>
2024-04-16 14:28:10 -0400

    adds hero section

--- src/app/page.js
+++ src/app/page.js
@@ -1,19 +1,41 @@
 import Image from 'next/image';
 import styles from './app.module.css';
+import nurseheart from '../../public/nurse-heart.svg'
 
+const imageStyle = {
+  borderRadius: '12px',
+}
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className={styles.description}> </div>
+      <section className={styles.heroSection}>
+        <div className={styles.leftCol}>
+          <h1>Medical care simplified for everyone</h1>
+          <p>Personalized care delivered in your home by nurses in your community</p>
+          <div className={styles.waitlistBox}>
+            <h2>Join the Waitlist</h2>
+            <div>
+              <button className={styles.button}>Nurses</button>
+              <button className={styles.buttonPurple}>Patients</button>
+            </div>
+          </div>
+        </div>
+        <div className={styles.rightCol}>
+          <div className={styles.heroImage}>
+            <Image 
+              src={nurseheart} 
+              sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw" 
+              alt="smiling nurse with hands in a heart shape" 
+              style={imageStyle}
+            />
+          </div>
+        </div>
+      </section>
+      <section className={styles.featuresSection}>
 
-      <div className={styles.center}>
-        
-      </div>
-
-      <div className={styles.grid}>
+      </section>
       
-      </div>
     </main>
   );
 }
